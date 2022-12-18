e10aec4d05152932f0e22215c9955e97fa235c38
chaolic6505 <chaolic6505@gmail.com>
2022-12-17 22:19:05 -0800

    Initial Commit

---
+++ To Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To Do
+//
+//  Created by SC on 2022-12-17.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
